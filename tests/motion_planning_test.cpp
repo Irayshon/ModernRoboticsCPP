3436225fb925a283eb5b697cc9a86f6451ad3c2e
Irayshon <ra4luu@gmail.com>
2026-02-02 19:57:10 +0100

    test: expand motion planning placeholder coverage

--- tests/motion_planning_test.cpp
+++ tests/motion_planning_test.cpp
@@ -2,7 +2,11 @@
 
 #include <gtest/gtest.h>
 
+#include <type_traits>
+
 TEST(MotionPlanningTest, Placeholder) {
+  static_assert(std::is_default_constructible<mymr::MotionPlanning>::value,
+                "MotionPlanning should be default-constructible");
   mymr::MotionPlanning plan;
   (void)plan;
 }
