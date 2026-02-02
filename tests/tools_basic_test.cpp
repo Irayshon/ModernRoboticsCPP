a77bce8db810897801ba50d895c6b5a471ff4be4
Irayshon <ra4luu@gmail.com>
2026-02-02 19:21:31 +0100

    test: expand tools coverage

--- tests/tools_basic_test.cpp
+++ tests/tools_basic_test.cpp
@@ -16,3 +16,21 @@ TEST(ToolsBasicTest, NearZeroAndNormalize) {
   auto n = mymr::Tools::Normalize(v);
   EXPECT_NEAR(n.norm(), 1.0, 1e-9);
 }
+
+TEST(ToolsBasicTest, ProjectToSo3Se3Validity) {
+  Eigen::Vector3d w(1.0, 2.0, 3.0);
+  w.normalize();
+  Eigen::Matrix3d so3 = mymr::Tools::VecToso3(w * 0.6);
+  Eigen::Matrix3d R = mymr::Tools::MatrixExp3(so3);
+  Eigen::Matrix3d R_bad = R;
+  R_bad(0, 0) += 0.02;
+  R_bad(1, 2) -= 0.01;
+
+  Eigen::Matrix3d R_proj = mymr::Tools::ProjectToSO3(R_bad);
+  EXPECT_TRUE(mymr::Tools::TestIfSO3(R_proj));
+
+  Eigen::Vector3d p(0.3, -0.2, 0.5);
+  Eigen::MatrixXd T_bad = mymr::Tools::RpToTrans(R_bad, p);
+  Eigen::MatrixXd T_proj = mymr::Tools::ProjectToSE3(T_bad);
+  EXPECT_TRUE(mymr::Tools::TestIfSE3(T_proj));
+}
--- tests/tools_jacobian_test.cpp
+++ tests/tools_jacobian_test.cpp
@@ -16,3 +16,14 @@ TEST(ToolsJacobianTest, JacobianShapes) {
   EXPECT_EQ(Js.rows(), 6);
   EXPECT_EQ(Js.cols(), 2);
 }
+
+TEST(ToolsJacobianTest, ConditionNumberAndSingularity) {
+  Eigen::Matrix2d J = Eigen::Matrix2d::Identity();
+  EXPECT_NEAR(mymr::Tools::ConditionNumber(J), 1.0, 1e-9);
+  EXPECT_FALSE(mymr::Tools::IsSingular(J));
+
+  Eigen::Matrix2d J_bad = Eigen::Matrix2d::Identity();
+  J_bad(1, 1) = 1e-8;
+  EXPECT_GT(mymr::Tools::ConditionNumber(J_bad), 1e6);
+  EXPECT_TRUE(mymr::Tools::IsSingular(J_bad));
+}
